368778d263d8178bf1ac877f1878650cd249fb93
hagiionutnichi <hagiionutnichi@gmail.com>
2021-01-26 21:32:45 +0000

    Kernel Load

--- gnu-efi/bootloader/main.c
+++ gnu-efi/bootloader/main.c
@@ -1,9 +1,35 @@
 #include <efi.h>
 #include <efilib.h>
+EFI_FILE* LoadFile(EFI_FILE* Directory, CHAR16* Path, EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable)
+{
+	EFI_FILE* Result;
+
+	EFI_LOADED_IMAGE_PROTOCOL* LoadedImage;
+	SystemTable->BootServices->HandleProtocol(ImageHandle, &gEfiLoadedImageProtocolGuid, (void**)&LoadedImage);
+
+	EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* FileSystem;
+	SystemTable->BootServices->HandleProtocol(LoadedImage->DeviceHandle, &gEfiSimpleFileSystemProtocolGuid, (void**)&FileSystem);
+
+	if(Directory == NULL) {
+		FileSystem->OpenVolume(FileSystem, &Directory);
+	}
+
+	EFI_STATUS status = Directory->Open(Directory, &Result, Path, EFI_FILE_MODE_READ, EFI_FILE_READ_ONLY);
+
+	if(status != EFI_SUCCESS) {
+		return NULL;
+	}
+
+	return Result;
+}
 
 EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
 	InitializeLib(ImageHandle, SystemTable);
-	Print(L"Initializing Kernel...");
+	Print(L"Initializing Kernel...\n\r");
+
+	if(LoadFile(NULL, L"kernel.elm", ImageHandle, SystemTable)) {
+		Print(L"Kernel Loaded!\n\r");
+	}
 	
 	return EFI_SUCCESS; // Exit the UEFI application
 }
