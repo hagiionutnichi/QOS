d676295821c52b4d4be2410df7bcbbbb3441e950
hagiionutnichi <hagiionutnichi@gmail.com>
2021-02-06 21:12:50 +0000

    GDT and NASM Assembler

---
+++ kernel/src/GDT/gdt.cpp
@@ -0,0 +1,11 @@
+#include "gdt.h"
+
+__attribute__((aligned(0x1000)))
+GDT DefaultGDT = {
+    {0, 0, 0, 0x00, 0x00, 0}, //Null
+    {0, 0, 0, 0x9a, 0xa0, 0}, //Kernel Code
+    {0, 0, 0, 0x92, 0xa0, 0}, //Kernel Data
+    {0, 0, 0, 0x00, 0x00, 0}, //User Null
+    {0, 0, 0, 0x9a, 0xa0, 0}, //User Code
+    {0, 0, 0, 0x92, 0xa0, 0} //User Data
+};
